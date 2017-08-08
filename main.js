45e56adb7384cde7cf68c10d84fd0a108d25d1e4
Navroop <gillnavi926@gmail.com>
2017-08-08 18:47:19 +0530

    abt to start  variant

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ foodieApp.controller('loginController',function($scope,$location) {
 })
 
 
-foodieApp.controller('restaurantController',function($scope,$routeParams) {
+foodieApp.controller('restaurantController',function($scope,$routeParams,$http) {
 	$scope.restaurantId = $routeParams.id;
 	var restaurants = [{
 	name: 'Farzi Cafe',
@@ -29,7 +29,11 @@ foodieApp.controller('restaurantController',function($scope,$routeParams) {
 	cuisines: 'Modern Indian',
 	cost: '2200',
 	hours: '12 Noon to 1 AM (Mon-Sun)',
-	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
+	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg',
+	bestDish: {
+	name: 'Corn Pizza',
+	image: 'http://noblepig.com/images/2016/06/Avocado-and-Three-Bean-Salad-is-perfect-for-a-summertime-barbecue-side-dish.JPG'
+}
 },
 {
 	name: 'Priates Cafe',
@@ -54,7 +58,42 @@ foodieApp.controller('restaurantController',function($scope,$routeParams) {
 	image: 'https://www.parktheatre.co.uk/media/images/coffee-cake.jpg'
 }];
 	$scope.restaurant = restaurants[$routeParams.id - 1];
+	$scope.getIngredients = function(url) {
+		var data = '{"inputs":[{"data":{"image":{"url":"' + url + '"}}}]}'
+$http({
+	'method': 'POST',
+	'url': 'https://api.clarifai.com/v2/models/bd367be194cf45149e75f01d59f77ba7/outputs',
+	'headers': {
+		'Authorization': 'Key fbdc6327653d422ab95c17cfc65b7fb9',
+		'Content-Type': 'application/json'
+	},
+	'data': data,
+}).then(function (response) {
+			var ingredients = response.data.outputs[0].data.concepts;
+			$scope.ingredients = [];
+ 			for (var i =0;i < ingredients.length;i++) {
+  				$scope.ingredients.push(ingredients[i].name);
+  			}
+    		// $('.ingredients').html(list);
+				// console.log(list);
+    		}, function (xhr) {
+        	console.log(xhr);
+  });
+	// success: function (response) {
+	// 	var ingredients = response.outputs[0].data.concepts;
+	// 		var list = '';
+	// 		for (var i =0;i < ingredients.length ;i++) {
+	// 			list += 'div class="ingredient">' + ingredients[i].name + '</div>'
+	// 		}
+	// 	$('.ingredients').html(list);
+  //   },
+  //   error: function (xhr) {
+  //   	console.log(xhr);
+  //   }
+}
 })
+
+
 
 
 
